d16df2f564f126602327798527b8b08c4b60ec32
ladbakare <116469637+ladbakare@users.noreply.github.com>
2023-04-12 16:42:53 -0500

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,13 +1,10 @@
+
 module "s3-bucket" {
   source  = "app.terraform.io/bakare-work/s3-bucket/aws"
   version = "2.8.0"
-}
- bucket_prefix = "ola-bakare"
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
+  
+  bucket_prefix = "ola-bakare"
+   versioning = {
     enabled = true
   }
-
 }
